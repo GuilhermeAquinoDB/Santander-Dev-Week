4ca577156c332f6b2301f4b554c8373e0fc7ea33
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-05-29 14:02:04 -0300

    Validation

--- src/main/java/com/project/model/dto/StockDTO.java
+++ src/main/java/com/project/model/dto/StockDTO.java
@@ -2,6 +2,11 @@ package com.project.model.dto;
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,9 +15,21 @@ import lombok.Setter;
 public class StockDTO {
 
 	private Long id;
+	
+	@NotNull
 	private String name;
+	
+	@NotNull
+	@DecimalMin(value = "0.00")
+	@Digits(integer = 6, fraction = 2)
 	private Double price;
+	
+	@NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
 	private LocalDate date;
+	
+	@NotNull
+	@Digits(integer = 3, fraction = 2)
 	private Double variation;
 
 }
