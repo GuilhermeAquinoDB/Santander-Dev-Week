3e3d7404e26ea8f8e29b46307963a0307f02820b
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-06-12 16:53:15 -0300

    component StockCard

--- front/homebroker/src/app/app.module.ts
+++ front/homebroker/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
     BrowserModule,
     AppRoutingModule,
     DashboardModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
